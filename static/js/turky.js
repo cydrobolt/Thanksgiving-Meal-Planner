be40fece14302b10bfc7e0f081fc86abd5d6cba4
Max Mitchell <mmitchell4488@yahoo.com>
2015-11-21 12:56:42 -0500

    most pages added

--- static/js/turky.js
+++ static/js/turky.js
@@ -1,16 +1,59 @@
 
 
+var pages = [$('#loginPage'), $('#eventsPage'), $('#createEventPage'), $('#recipiesPage'), $('#viewRecipiePage'), $('#editRecipiePage')];
+
+
 $('#navEvents').on('click', function(e) {
 	e.preventDefault();
-	$('#loginPage').hide();
-	$('#eventsPage').show();
+	for (var i = 0; i < pages.length; i++) {
+		if (i !== 1) {
+			pages[i].hide();
+		} else {
+			pages[i].show();
+		}
+	}
 });
 
 
 $('#navHome').on('click', function(e) {
 	e.preventDefault();
-	$('#loginPage').show();
-	$('#eventsPage').hide();
+	for (var i = 0; i < pages.length; i++) {
+		if (i !== 0) {
+			pages[i].hide();
+		} else {
+			pages[i].show();
+		}
+	}
 });
 
-$('#eventsPage').hide();
+$('#navRecipies').on('click', function(e) {
+	e.preventDefault();
+	for (var i = 0; i < pages.length; i++) {
+		if (i !== 3) {
+			pages[i].hide();
+		} else {
+			pages[i].show();
+		}
+	}
+});
+
+$('#createEventBtn').on('click', function(e) {
+	e.preventDefault();
+	for (var i = 0; i < pages.length; i++) {
+		if (i !== 2) {
+			pages[i].hide();
+		} else {
+			pages[i].show();
+		}
+	}
+});
+
+
+
+for (var i = 0; i < pages.length; i++) {
+		if (i !== 0) {
+			pages[i].hide();
+		} else {
+			pages[i].show();
+		}
+	}
