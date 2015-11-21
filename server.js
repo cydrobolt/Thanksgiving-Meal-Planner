1fb3c0aa9dbbad6bddca8ce1f240f3f46de442a6
Grace Zhang <gz.gracez16@gmail.com>
2015-11-20 22:15:40 -0500

    header partial

--- server.js
+++ server.js
@@ -2,22 +2,16 @@ var fs = require("fs");
 var express = require('express');
 var path = require('path');
 var app = express();
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
 var ejs = require("ejs");
 
-app.use(bodyParser.json());  
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/static", express.static('static'));
 app.set('view engine', 'ejs');
 
-
 app.get("/", function(req,res){
-
-
-
-res.render("index.ejs");
-
-
+	res.render("index.ejs");
 });
 
 app.listen(process.env.PORT || 5000);
