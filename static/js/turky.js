4c8fb8794d0147bccc2249ea454369f6df020a0a
Max Mitchell <mmitchell4488@yahoo.com>
2015-11-21 12:38:53 -0500

    w/ events page

--- static/js/turky.js
+++ static/js/turky.js
@@ -6,4 +6,11 @@ $('#navEvents').on('click', function(e) {
 	$('#eventsPage').show();
 });
 
+
+$('#navHome').on('click', function(e) {
+	e.preventDefault();
+	$('#loginPage').show();
+	$('#eventsPage').hide();
+});
+
 $('#eventsPage').hide();
