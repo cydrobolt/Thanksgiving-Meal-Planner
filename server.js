b28767fae9055b3ae1442313cd2e816106e6f2fb
Sam Schiff <Sam Schiff>
2015-11-21 20:59:27 -0500

    almost have google working

--- server.js
+++ server.js
@@ -93,6 +93,13 @@ app.get('/profile', isLoggedIn, function(req, res) {
     }));
 
 
+       app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+  app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/'
+            }));
 
 
 function isLoggedIn(req, res, next) {
